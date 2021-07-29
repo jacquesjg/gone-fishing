2e82de87bd627ba9e5f19b2609524f3764120566
jacquesjg <jjeangi@gmail.com>
2021-07-29 17:56:56 +0200

    edits

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const adjArray1 = ['red', 'yellow', 'blue', 'orange', 'red', 'violet', 'indigo',
 
 const adjArray2 = ['-finned', '-backed', '-tinted', ' saltwater', '-gilled', '-colored', '-toothless', '-european', '-american', ' tropical']
 
-const nameArray = [' trout', ' bass', ' pike', ' perch', ' salmon', ' catfish', ' herring', ' carp', ' swordfish', ' tuna']
+const nameArray = [' trout', ' bass', ' pike', ' perch', ' salmon', ' catfish', ' herring', ' carp', ' swordfish', ' tuna']
 
 
 ///==========================
 // make weight between 0.00 and 10.00
-function randomWeight() { 
+function randomWeight() {
     return (Math.round((Math.random() * 1000)) / 100)
 }
 
@@ -32,20 +32,20 @@ function randomValue() {
 let formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-  });
-  
+});
+
 
 ///==========================
 // make random fish
 function randChoice(array) {
     return array[Math.floor(array.length * Math.random())]
-  }
+}
 
-  function randomFishName() {
+function randomFishName() {
     const fishName = randChoice(adjArray1) + randChoice(adjArray2) + randChoice(nameArray);
     return fishName
 }
- 
+
 
 ///=========================
 // make random fish object
@@ -68,8 +68,8 @@ function timeConvert(n) {
     const minutes = (hours - rhours) * 60;
     const rminutes = Math.round(minutes);
     return rhours + " hour(s) and " + rminutes + " minute(s).";
-    }
-    
+}
+
 ///==========================
 // let user exit game / break loop
 let gameover = 0;
@@ -77,12 +77,17 @@ let gameover = 0;
 
 ///==========================
 //  while loop
-    while (gameover === 0) {
-        let userInput = prompt("You've gone fishing! Try to maximize the value of your caught fish. You can fish for six hours (till 12:00pm) and can catch at most 10 lbs of fish ===================================== The time is 6:00am. So far you've caught: 0 fish, 0 lbs, $0.00");
-            if (userInput === 'exit'){
-                gameover += 1
-            }
-        }
-       if (gameover === 1) {
-        console.log('Game Over!')
+while (gameover === 0) {
+
+    let userInput = prompt("You've gone fishing! Try to maximize the value of your caught fish. You can fish for six hours (till 12:00pm) and can catch at most 10 lbs of fish ===================================== The time is 6:00am. So far you've caught: 0 fish, 0 lbs, $0.00");
+    if (userInput === 'exit') {
+        gameover += 1
     }
+    console.log(randomFish())
+}
+if (gameover === 1) {
+    console.log('Game Over!')
+}
+
+
+
